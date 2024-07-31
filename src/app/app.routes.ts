4baf4f06e34ac2932aa2fd19af5d4c6c467df4d3
Guilherme Lopes <guilhermecarvlinkedin@gmail.com>
2024-07-30 22:56:48 -0300

    feat: Criação das rotas das páginas e permintindo atribuir valores pelas rotas para os Inputs e Outputs dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'error'
+    },
+    {
         path: 'error',
-        pathMatch: 'full',
-        component: ErrorComponent
+        component: ErrorComponent,
+        data: {
+            message: 'ESTAMOS FORA DO AR, JÁ ESTAREMOS DE VOLTA',
+            buttonMessage: 'Voltar a tela anterior'
+        }
     }
 ];
