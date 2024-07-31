987081e074a2d2eeb876527023d21aa3554da71e
Guilherme Lopes <guilhermecarvlinkedin@gmail.com>
2024-07-30 22:53:16 -0300

    feat: Criando Componente de Tela de Erro

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+  styleUrl: './error.component.scss'
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
+  @Input() message?: string
+  @Input() buttonMessage?: string
+  @Output() buttonAction?: EventEmitter<any> = new EventEmitter()
 
+  ngOnInit() {
+    this.buttonMessage = this.buttonMessage ?? 'Voltar'
+  }
+
+  buttonClicked() {
+    if(this.buttonAction && this.buttonAction.observed)
+      this.buttonAction.emit()
+    else
+      window.history.back()
+  }
 }
